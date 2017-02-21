a9308e87210de7775e18c1063df40f45b1a23942
Gregory Buchenberger <gregory.buchenberger@charter.com>
2017-02-20 18:56:30 -0700

    clean up

--- scripts/controllers/import-gitlab.js
+++ scripts/controllers/import-gitlab.js
@@ -10,46 +10,87 @@ SwaggerEditor.controller('GitlabImportCtrl', function FileImportCtrl($scope,
   const token = Preferences.get('gitlabToken');
   const projectsUrl = baseurl + "/api/v3/projects?private_token=" + token;
   var results;
-
+  $scope.projectSelected = false;
+  $scope.branchSelected = false;
   $scope.projectid = null;
   $scope.ref = null;
   $scope.filepath = null;
   $scope.error = null;
-  $scope.opts = {
-    useProxy: false
+  // $scope.opts = {
+  //   useProxy: false
+  // };
+
+  // $scope.treeOptions = {
+  //   nodeChildren: "children",
+  //   dirSelectable: true,
+  //   injectClasses: {
+  //     ul: "a1",
+  //     li: "a2",
+  //     liSelected: "a7",
+  //     iExpanded: "a3",
+  //     iCollapsed: "a4",
+  //     iLeaf: "a5",
+  //     label: "a6",
+  //     labelSelected: "a8"
+  //   }
+  // };
+
+  // $scope.dataForTheTree =
+  // [
+  //   {name: "Joe", age: "21", children: [
+  //       {name: "Smith", age: "42", children: []},
+  //       {name: "Gary", age: "21", children: [
+  //           {name: "Jenifer", age: "23", children: [
+  //               {name: "Dani", age: "32", children: []},
+  //               {name: "Max", age: "34", children: []}
+  //           ]}
+  //       ]}
+  //   ]},
+  //   {name: "Albert", age: 33, children: []},
+  //   {name: "Ron", age: "29", children: []}
+  // ];
+
+  var fetchBranches = function() {
+    var branchesUrl = baseurl +
+    "/api/v3/projects/" +
+    $scope.selectedProject.id +
+    "/repository/branches?private_token=" +
+    token;
+    $http({
+      method: 'GET',
+      url: branchesUrl
+    }).then(function successCallback(response) {
+      $scope.projectSelected = true;
+      $scope.branches = _.map(response.data, function(branch) {
+        return {name: branch.name};
+      });
+    }, function errorCallback(response) {
+      console.error(`Error retrieving projects ${response}`);
+    });
   };
 
-  $scope.treeOptions = {
-    nodeChildren: "children",
-    dirSelectable: true,
-    injectClasses: {
-      ul: "a1",
-      li: "a2",
-      liSelected: "a7",
-      iExpanded: "a3",
-      iCollapsed: "a4",
-      iLeaf: "a5",
-      label: "a6",
-      labelSelected: "a8"
-    }
+  var fetchFiletree = function() {
+    $scope.branchSelected = true;
+    var filesUrl = baseurl +
+    "/api/v3/projects/" +
+    $scope.selectedProject.id +
+    "/repository/tree?recursive=true&ref_name=" +
+    $scope.selectedBranch.name +
+    "&private_token=" +
+    token;
+    $http({
+      method: 'GET',
+      url: filesUrl
+    }).then(function successCallback(response) {
+      $scope.filetree = _.filter(response.data, function(file) {
+        return (file.path.endsWith(".json") || file.path.endsWith(".yaml"));
+      });
+      console.log($scope.filetree);
+    }, function errorCallback(response) {
+      console.error(`Error retrieving file tree ${response}`);
+    });
   };
 
-  $scope.dataForTheTree =
-  [
-    {name: "Joe", age: "21", children: [
-        {name: "Smith", age: "42", children: []},
-        {name: "Gary", age: "21", children: [
-            {name: "Jenifer", age: "23", children: [
-                {name: "Dani", age: "32", children: []},
-                {name: "Max", age: "34", children: []}
-            ]}
-        ]}
-    ]},
-    {name: "Albert", age: 33, children: []},
-    {name: "Ron", age: "29", children: []}
-  ];
-
-  // Simple GET request example:
   $http({
     method: 'GET',
     url: projectsUrl
@@ -57,7 +98,7 @@ SwaggerEditor.controller('GitlabImportCtrl', function FileImportCtrl($scope,
     $scope.items = _.map(response.data, function(project) {
       return {name: project.name, id: project.id};
     });
-    console.log($scope.items);
+    // console.log($scope.items);
   }, function errorCallback(response) {
     console.log(response);
   });
@@ -98,6 +139,7 @@ SwaggerEditor.controller('GitlabImportCtrl', function FileImportCtrl($scope,
     }
     $uibModalInstance.close();
   };
-
+  $scope.fetchFiletree = fetchFiletree;
+  $scope.fetchBranches = fetchBranches;
   $scope.cancel = $uibModalInstance.close;
 });
