522631e944d09dae48d13ec2eee963c8d50ceaf0
Gregory Buchenberger <buchenberger@gmail.com>
2017-04-19 19:50:26 -0600

    gh-pages fix

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,29 +1,22 @@
 '#!/bin/bash'
 
-<<<<<<< HEAD
-=======
 echo 'checkout master'
 git checkout master
 
 echo 'pull origin master'
 git pull origin master
 
->>>>>>> origin/master
 echo '>> rm -rf node_modules'
 rm -rf node_modules
 
-echo '>> npm install'
-<<<<<<< HEAD
-npm install
+echo '>> yarn'
+yarn
 
 echo '>> npm run build'
 npm run build
 
 echo '>> npm test'
 npm test
-=======
-yarn
->>>>>>> origin/master
 
 echo '>> git checkout gh-pages'
 git checkout gh-pages
@@ -40,12 +33,6 @@ git add -A
 echo '>> git commit -m "Update from master at $(git rev-parse --short master)"'
 git commit -m "Update from master at $(git rev-parse --short master)"
 
-<<<<<<< HEAD
-=======
-echo '>> npm test'
-npm test
-
->>>>>>> origin/master
 echo '>> git push origin gh-pages'
 git push origin gh-pages
 
