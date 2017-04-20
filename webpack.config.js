6f108cbee31bb27fd2cb673ef4c2464fdf0c7559
Gregory Buchenberger <buchenberger@gmail.com>
2017-04-19 19:55:46 -0600

    gh fixes

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,9 @@ var config = {
   },
 
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'bundles'),
     filename: 'bundle.js',
-    publicPath: 'dist/'
+    publicPath: 'bundles/'
   },
 
   resolve: {
@@ -95,14 +95,6 @@ if (argv.production) {
   config.plugins.unshift(new webpack.optimize.UglifyJsPlugin({mangle: true}));
   config.plugins.unshift(new NgAnnotatePlugin({add: true}));
   config.plugins.unshift(new webpack.NoErrorsPlugin());
-}
-
-// if --electron is passed, modify the base href
-if (argv.electron) {
-  console.info('Electron build. This might work...');
-
-  config.output.path = path.join(__dirname, 'bundles');
-  config.output.publicPath = 'bundles/';
 }
 
 module.exports = config;
